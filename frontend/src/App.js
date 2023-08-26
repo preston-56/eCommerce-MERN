f1304e436ff27734567f0ce57813ed314bb0090b
Preston <prestonosoro56@gmail.com>
2023-08-27 00:48:35 +0300

    import Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './component/header/header'
 import './App.css';
 
 function App() {
   return (
     <div className='bg-red-500'>
-    React app
+      <Header/>
     </div>
   );
 }
