494f4071ea7c347b12ca0e325b289b2b332f99c4
Preston <prestonosoro56@gmail.com>
2023-08-27 01:12:40 +0300

    import logo and style component

--- frontend/src/component/header/header.jsx
+++ frontend/src/component/header/header.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import logo from "../../assets/images/logo.png";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="fixed shadow-md w-full h-16 px-2 md:px-4">
+      {/* desktop */}
+      <div className="flex items-center h-full">
+        <div className="h-16">
+          <img src={logo} className="h-full" alt="icon" />
+        </div>
+      </div>
+      {/* mobile */}
+    </header>
+  );
+};
 
-export default Header
+export default Header;
