ccb7aef632dc8fceee68662c9b03497d54a452a3
Preston <prestonosoro56@gmail.com>
2023-08-27 19:16:46 +0300

    define handleOnChange

---
+++ frontend/src/component/signup/signup.jsx
@@ -0,0 +1,125 @@
+import React, { useState } from "react";
+import loginSignupImage from "../../assets/images/login.gif";
+import { BiHide, BiShow } from "react-icons/bi";
+import { Link } from "react-router-dom";
+
+const SignUp = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  console.log(data);
+  const handleShowPassword = () => {
+    setShowPassword((preve) => !preve);
+  };
+
+  const handleShowConfirmPassword = () => {
+    setShowConfirmPassword((preve) => !preve);
+  };
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target; // Destructure name and value from e.target
+    setData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  return (
+    <div className="p-2 md:p-4">
+      <div className="w-full max-w-sm bg-white m-auto flex flex-col p-4">
+        {/* <h1 className='text-center text-2xl font-bold'>Sign up</h1> */}
+        <div className="w-20 overflow-hidden rounded-full drop-shadow-md m-auto ">
+          <img src={loginSignupImage} className="w-full" alt=".icon" />
+        </div>
+        <form className="w-full py-3 flex flex-col">
+          <label htmlFor="firstName">First Name</label>
+          <input
+            type={"text"}
+            id="firstName"
+            name="firstName"
+            className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"
+            value={data.firstName}
+            onChange={handleOnChange}
+          />
+
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            type={"text"}
+            id="lastName"
+            name="lastName"
+            className="mt-1 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"
+            value={data.lastName}
+            onChange={handleOnChange}
+          />
+
+          <label htmlFor="email">Email</label>
+          <input
+            type={"email"}
+            id="email"
+            name="email"
+            className="mt-1 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"
+            value={data.email}
+            onChange={handleOnChange}
+          />
+
+          <label htmlFor="password">Password</label>
+          <div className="flex px-2 py-1 bg-slate-200 rounded mt-1 mb-2 focus-within:outline focus-within:outline-blue-300">
+            <input
+              type={showPassword ? "text" : "password"}
+              id="password"
+              name="password"
+              className="w-full bg-slate-200 border-none outline-none"
+              value={data.password}
+              onChange={handleOnChange}
+            />
+            <span
+              className="flex text-xl cursor-pointer"
+              onClick={handleShowPassword}
+            >
+              {" "}
+              {showPassword ? <BiShow /> : <BiHide />}
+            </span>
+          </div>
+
+          <label htmlFor="confirmpassword">Confirm Password</label>
+          <div className="flex px-2 py-1 bg-slate-200 rounded mt-1 mb-2 focus-within:outline focus-within:outline-blue-300">
+            <input
+              type={showConfirmPassword ? "text" : "confirmpassword"}
+              id="confirmpassword"
+              name="confirmpassword"
+              className="w-full bg-slate-200 border-none outline-none"
+              value={data.confirmPassword}
+              onChange={handleOnChange}
+            />
+            <span
+              className="flex text-xl cursor-pointer"
+              onClick={handleShowConfirmPassword}
+            >
+              {" "}
+              {showConfirmPassword ? <BiShow /> : <BiHide />}
+            </span>
+          </div>
+          <button className="w-full max-w-[150px] m-auto bg-blue-500 hover:bg-blue-600 text-white text-xl font-medium text-center py-1 rounded-full mt-4">
+            Sign up
+          </button>
+        </form>
+        <p className="text-left text-sm mt-2">
+          Already have an account ?{" "}
+          <Link to={"login"} className="text-blue-500 underline">
+            Login
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
