a162719922ab3b3126fb2d784be5428c04224965
Preston <prestonosoro56@gmail.com>
2023-08-27 16:32:12 +0300

    flex newproduct && login

--- frontend/src/component/header/header.jsx
+++ frontend/src/component/header/header.jsx
@@ -37,7 +37,7 @@ const Header = () => {
               <HiOutlineUserCircle />
             </div>
             {showMenu && (
-              <div className="absolute right-2 bg-white py-2 px-2 shadow drop-shadow-md">
+              <div className="absolute right-2 bg-white py-2 px-2 shadow drop-shadow-md flex flex-col">
                 <Link to={"newproduct"} className="whitespace-nowrap cursor-pointer">New Product</Link>
                 <Link to={"login"} className="whitespace-nowrap cursor-pointer">Login</Link>
               </div>
