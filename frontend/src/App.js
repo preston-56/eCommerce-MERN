4e9642f21dd0f53301e2df0378a703417bc3eb24
Preston <prestonosoro56@gmail.com>
2023-08-27 03:26:00 +0300

    style Outlet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './component/header/header'
 import './App.css';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <div className=''>
       <Header/>
+      <main className='pt-16'>
+        <Outlet/>
+      </main>
     </div>
   );
 }
