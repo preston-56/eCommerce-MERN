1489156d98b5a820517e5a1aa6b264bc0ddd686a
Preston <prestonosoro56@gmail.com>
2023-08-27 10:58:57 +0300

    login route path

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,28 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom';
-import Home from './component/home/home';
-import Menu from './component/menu/menu';
-import About from './component/about/about';
-import Contact from './component/contact/contact';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./component/home/home";
+import Menu from "./component/menu/menu";
+import About from "./component/about/about";
+import Contact from "./component/contact/contact";
+import Login from "./component/login/login";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<App/>}>
-      <Route index element={<Home/>}/>
-      <Route path='menu' element={<Menu/>}/>
-      <Route path='about' element={<About/>}/>
-      <Route path='contact' element={<Contact/>}/>
+    <Route path="/" element={<App />}>
+      <Route index element={<Home />} />
+      <Route path="menu" element={<Menu />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="login" element={<Login />} />
     </Route>
-
   )
-)
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <RouterProvider router={router}/>
 );
 
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={router} />);
