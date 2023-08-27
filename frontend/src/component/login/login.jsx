bef16068fa31830219b9856938de0dcfd594db85
Preston <prestonosoro56@gmail.com>
2023-08-27 21:09:15 +0300

    handleShowPassword|handleOnchange|handleSubmit

--- frontend/src/component/login/login.jsx
+++ frontend/src/component/login/login.jsx
@@ -1,9 +1,87 @@
-import React from 'react'
+import React, { useState } from "react";
+import { BiHide, BiShow } from "react-icons/bi";
+import { Link } from "react-router-dom";
+import logo from "../../assets/images/login.gif";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [showPassword, setShowPassword] = useState(false);
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+  console.log(data);
+  const handleShowPassword = () => {
+    setShowPassword((preve) => !preve);
+  };
 
-export default Login
+  const handleOnChange = (e) => {
+    const { name, value } = e.target; // Destructure name and value from e.target
+    setData((preve) => {
+      return {
+        ...preve,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { email, password } = data;
+    if (email && password) {
+      alert("successfull");
+    } else {
+      alert("Please enter required fields");
+    }
+  };
+  return (
+    <div className="p-2 md:p-4">
+      <div className="w-full max-w-sm bg-white m-auto flex flex-col p-4">
+        {/* <h1 className='text-center text-2xl font-bold'>Sign up</h1> */}
+        <div className="w-20 overflow-hidden rounded-full drop-shadow-md m-auto ">
+          <img src={logo} className="w-full" alt=".icon" />
+        </div>
+        <form className="w-full py-3 flex flex-col" onSubmit={handleSubmit}>
+          <label htmlFor="email">Email</label>
+          <input
+            type={"email"}
+            id="email"
+            name="email"
+            className="mt-1 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"
+            value={data.email}
+            onChange={handleOnChange}
+          />
+
+          <label htmlFor="password">Password</label>
+          <div className="flex px-2 py-1 bg-slate-200 rounded mt-1 mb-2 focus-within:outline focus-within:outline-blue-300">
+            <input
+              type={showPassword ? "text" : "password"}
+              id="password"
+              name="password"
+              className="w-full bg-slate-200 border-none outline-none"
+              value={data.password}
+              onChange={handleOnChange}
+            />
+            <span
+              className="flex text-xl cursor-pointer"
+              onClick={handleShowPassword}
+            >
+              {" "}
+              {showPassword ? <BiShow /> : <BiHide />}
+            </span>
+          </div>
+          <button className="w-full max-w-[150px] m-auto bg-blue-500 hover:bg-blue-600 text-white text-xl font-medium text-center py-1 rounded-full mt-4">
+            Login
+          </button>
+        </form>
+        <p className="text-left text-sm mt-2">
+          Don't have an account ?{" "}
+          <Link to={"/signup"} className="text-blue-500 underline">
+            Sign Up
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
