4432fd058ec0ebb3067863ec126c5aa6dd24fb96
Preston <prestonosoro56@gmail.com>
2023-08-27 03:27:08 +0300

    define route path

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom';
+import Home from './component/home/home';
+import Menu from './component/menu/menu';
+import About from './component/about/about';
+import Contact from './component/contact/contact';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App/>}>
-
+      <Route index element={<Home/>}/>
+      <Route path='menu' element={<Menu/>}/>
+      <Route path='about' element={<About/>}/>
+      <Route path='contact' element={<Contact/>}/>
     </Route>
 
   )
