e84e4ea6cab3fd75c855e716afc17fa6e26fed8c
Preston <prestonosoro56@gmail.com>
2023-08-27 10:49:22 +0300

    define showMenu && handleShowMenu

--- frontend/src/component/header/header.jsx
+++ frontend/src/component/header/header.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import logo from "../../assets/images/logo.png";
 import { Link } from "react-router-dom";
-import { FaUserAlt } from "react-icons/fa";
+import { HiOutlineUserCircle } from "react-icons/hi";
 import { BsCartFill } from "react-icons/bs";
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const handleShowMenu = () => {
+    setShowMenu(preve => !preve);
+  }
   return (
     <header className="fixed shadow-md w-full h-16 px-2 md:px-4 z-50">
       {/* desktop */}
@@ -24,10 +28,20 @@ const Header = () => {
           </nav>
           <div className="text-2xl text-slate-600 relative">
             <BsCartFill />
-            <div className="absolute -top-1 -right-1 text-white bg-red-500 h-4 w-4 rounded-full m-0 p-0 text-sm text-center">0</div>
+            <div className="absolute -top-1 -right-1 text-white bg-red-500 h-4 w-4 rounded-full m-0 p-0 text-sm text-center">
+              0
+            </div>
           </div>
-          <div className="text-2xl text-slate-600">
-            <FaUserAlt />
+          <div className="text-slate-600" onClick={handleShowMenu}>
+            <div className="text-3xl cursor-pointer">
+              <HiOutlineUserCircle />
+            </div>
+            {showMenu && (
+              <div className="absolute right-2 bg-white py-2 px-2 shadow drop-shadow-md">
+                <p className="whitespace-nowrap cursor-pointer">New Product</p>
+                <p className="whitespace-nowrap cursor-pointer">Login</p>
+              </div>
+            )}
           </div>
         </div>
       </div>
