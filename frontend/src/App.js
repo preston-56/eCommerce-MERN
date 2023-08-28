688327705eee17bf0726f44d26de60f40aa09383
Preston <prestonosoro56@gmail.com>
2023-08-28 18:46:14 +0300

    toast

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,20 @@ import React from "react";
 import Header from "./component/header/header";
 import "./App.css";
 import { Outlet } from "react-router-dom";
+// eslint-disable-next-line
+import toast, { Toaster } from "react-hot-toast";
 
 function App() {
   return (
-    <div className="">
-      <Header />
-      <main className="pt-16 bg-slate-100 min-h-[calc(100vh)]">
-        <Outlet />
-      </main>
-    </div>
+    <>
+      <Toaster />
+      <div className="">
+        <Header />
+        <main className="pt-16 bg-slate-100 min-h-[calc(100vh)]">
+          <Outlet />
+        </main>
+      </div>
+    </>
   );
 }
 
