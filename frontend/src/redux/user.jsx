1a665db8801c8518afad63f6fe4eca5334f7529a
Preston <prestonosoro56@gmail.com>
2023-08-29 15:42:35 +0300

    Redux|logout

--- frontend/src/redux/user.jsx
+++ frontend/src/redux/user.jsx
@@ -14,16 +14,23 @@ export const userSlice = createSlice({
   reducers: {
     loginRedux: (state, action) => {
       console.log(action.payload.data);
-    //   state.user = action.payload.data;
-    state._id = action.payload.data._id
-    state.firstName = action.payload.data.firstName
-    state.lastName = action.payload.data.lastName
-    state.email = action.payload.data.email
-    state.image = action.payload.data.image
+      //   state.user = action.payload.data;
+      state._id = action.payload.data._id;
+      state.firstName = action.payload.data.firstName;
+      state.lastName = action.payload.data.lastName;
+      state.email = action.payload.data.email;
+      state.image = action.payload.data.image;
+    },
+    logoutRedux: (state, action) => {
+      state._id = "";
+      state.firstName = "";
+      state.lastName = "";
+      state.email = "";
+      state.image = "";
     },
   },
 });
 
-export const { loginRedux } = userSlice.actions;
+export const { loginRedux,logoutRedux } = userSlice.actions;
 
 export default userSlice.reducer;
